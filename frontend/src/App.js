a16dff64233e6b63ba903347eaf8b78928a12a15
Oleksii Samutin <samutin63@gmail.com>
2022-05-28 11:33:16 +0300

    Added logout functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from "./components/LoginPage";
 import {
@@ -24,85 +24,90 @@ import BrowseUsersComponent from "./components/BrowseUsersComponent";
 import UserPageComponent from "./components/UserPageComponent";
 import UploadLabForUserComponent from "./components/UploadLabForUserComponent";
 const useStyles = makeStyles(theme => ({
-    root: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        flexGrow: 1,
-        spacing: 8
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    flexGrow: 1,
+    spacing: 8
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 
 function App() {
-    const [user,setUser] = useState(null)
-    const classes = useStyles();
-     useEffect(()=>{
-          const userPageComponent = JSON.parse(localStorage.getItem('user'))
-         if (userPageComponent != null) {
-             setUser(userPageComponent)
-         }
-     },[])
+  const [user, setUser] = useState(null)
+  const classes = useStyles();
+  useEffect(() => {
+    const userPageComponent = JSON.parse(localStorage.getItem('user'))
+    if (userPageComponent != null) {
+      setUser(userPageComponent)
+    }
+  }, [])
+  const logOut = () => {
+    localStorage.clear();
+    document.location.reload()
+  }
   return (
-      <Router>
-          <div className={classes.root}>
-              <AppBar position="static" >
-                  <Toolbar m={-12321} className={classes.root}>
-                      <Button className={classes.title} href="/lectures">Лекції</Button>
-                      <Button className={classes.title} href="/leaderboard">Рейтинг</Button>
-                      {user && (user.roles.includes("ROLE_TEACHER") || user.roles.includes("ROLE_REVIEWER"))&& <Button href="/evaluateLab">Оцінити лабораторні</Button>}
-                      {user && (user.roles.includes("ROLE_TEACHER") || user.roles.includes("ROLE_REVIEWER"))&& <Button href="/browse-users">Список учнів</Button>}
-                      {user ?<Button href="me">{user.login}</Button> :<Button href="login" color="inherit">Увійти</Button>}
-                  </Toolbar>
-              </AppBar>
-          </div>
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static" >
+          <Toolbar m={-12321} className={classes.root}>
+            <Button className={classes.title} href="/lectures">Лекції</Button>
+            <Button className={classes.title} href="/leaderboard">Рейтинг</Button>
+            {user && (user.roles.includes("ROLE_TEACHER") || user.roles.includes("ROLE_REVIEWER")) && <Button href="/evaluateLab">Оцінити лабораторні</Button>}
+            {user && (user.roles.includes("ROLE_TEACHER") || user.roles.includes("ROLE_REVIEWER")) && <Button href="/browse-users">Список учнів</Button>}
+            {user ? <Button onClick={logOut}>Вийти</Button> : null}
+            {user ? <Button href="me">{user.login}</Button> : <Button href="login" color="inherit">Увійти</Button>}
+          </Toolbar>
+        </AppBar>
+      </div>
 
-        <Switch>
-          <Route exact path="/">
-            <div>Home</div>
-          </Route>
-          <Route path="/about">
-            <div>About</div>
-          </Route>
-            <Route path="/leaderboard">
-                <LeaderboardComponent></LeaderboardComponent>
-          </Route>
-            <Route path="/user/:login/createLab">
-            <UploadLabForUserComponent></UploadLabForUserComponent>
+      <Switch>
+        <Route exact path="/">
+          <div>Home</div>
         </Route>
-            <Route path="/user/:login">
-                <UserPageComponent></UserPageComponent>
-          </Route>
-          <Route path="/lectures">
+        <Route path="/about">
+          <div>About</div>
+        </Route>
+        <Route path="/leaderboard">
+          <LeaderboardComponent></LeaderboardComponent>
+        </Route>
+        <Route path="/user/:login/createLab">
+          <UploadLabForUserComponent></UploadLabForUserComponent>
+        </Route>
+        <Route path="/user/:login">
+          <UserPageComponent></UserPageComponent>
+        </Route>
+        <Route path="/lectures">
           <LecturesPage></LecturesPage>
-          </Route>
-            <Route path="/browse-users">
+        </Route>
+        <Route path="/browse-users">
           <BrowseUsersComponent></BrowseUsersComponent>
-          </Route>
-            <Route path="/uploadLab">
+        </Route>
+        <Route path="/uploadLab">
           <UploadLabComponent></UploadLabComponent>
-          </Route>
-            <Route path="/evaluateLab">
+        </Route>
+        <Route path="/evaluateLab">
           <LabsPoolComponent></LabsPoolComponent>
-          </Route>
-          <Route path="/login">
-            <Login></Login>
-          </Route>
-            <Route path="/register">
-            <RegisterPage></RegisterPage>
-          </Route>
-            <Route path="/me">
-            <CurrentUserPage></CurrentUserPage>
-            </Route>
-            <Route path="/uploadLecture">
-            <LectureUploadingPage></LectureUploadingPage>
-          </Route>
-        </Switch>
-      </Router>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/register">
+          <RegisterPage></RegisterPage>
+        </Route>
+        <Route path="/me">
+          <CurrentUserPage></CurrentUserPage>
+        </Route>
+        <Route path="/uploadLecture">
+          <LectureUploadingPage></LectureUploadingPage>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
